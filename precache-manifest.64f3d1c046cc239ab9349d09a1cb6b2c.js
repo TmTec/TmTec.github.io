c670a6a71e765562ad39c3dab43fb9f1fc2a735b
TmTec <tm.x@foxmail.com>
2019-09-18 21:55:41 +0800

    v

---
+++ precache-manifest.64f3d1c046cc239ab9349d09a1cb6b2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "02497c27c70155ffd6d0",
+    "url": "/js/chunk-vendors.e6f7c778.js"
+  },
+  {
+    "revision": "969e36c6aa580f76515b",
+    "url": "/js/app.e5b062f8.js"
+  },
+  {
+    "revision": "99e924ed732f8eead7bd",
+    "url": "/js/about.251ffa6b.js"
+  },
+  {
+    "revision": "fe02ab6f94634691283a1979ca389351",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "969e36c6aa580f76515b",
+    "url": "/css/app.788f2a9b.css"
+  }
+];
